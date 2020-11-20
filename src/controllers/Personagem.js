dd6f157160e1b8e3fd89c5888386fd304c402a20
pedro <pedromf34@outlook.com>
2020-11-20 13:58:13 -0300

    Terminando as rotas

--- src/controllers/Personagem.js
+++ src/controllers/Personagem.js
@@ -1,46 +1,47 @@
 import db from '../database/connection';
 
+const valida = (name, age, condition, nationality, genre) => {
+  const erros = [];
+
+  if (name === '' || condition === '' || nationality === '' || genre === '') {
+    erros.push('Nenhum dos campos pode ser vazio');
+  }
+
+  if (typeof age !== 'number') {
+    erros.push('Idade tem que ser um número');
+  }
+
+  return erros;
+};
+
 class PersonagemController {
   // eslint-disable-next-line class-methods-use-this
   async index(req, res) {
     const trx = await db.transaction();
-
     try {
-      const personagens = await trx('personagem')
-        .select('id', 'name', 'age', 'condition', 'nationality', 'genre');
-
+      const personagens = await trx('personagem').whereExists(function () {
+        this.select('*').from('personagem');
+      });
       await trx.commit();
 
-      return res.send({
+      return res.status(200).send({
         personagens,
       });
     } catch (e) {
       await trx.rollback();
-      return res.send({ erro: 'Erro ao listar alunos' });
+      return res.status(400).json({ erro: 'Erro ao listar alunos' });
     }
   }
 
   async create(req, res) {
     const {
-      name,
-      age,
-      condition,
-      nationality,
-      genre,
+      name, age, condition, nationality, genre,
     } = req.body;
 
-    const erros = [];
-
-    if (name === '' || condition === '' || nationality === '' || genre === '') {
-      erros.push('Nenhum dos campos pode ser vazio');
-    }
-
-    if (typeof age !== 'number') {
-      erros.push('Idade tem que ser um número');
-    }
+    const erros = valida(name, age, condition, nationality, genre);
 
     if (erros.length > 0) {
-      return res.send({
+      return res.status(403).json({
         erros: erros.map((erro) => erro),
       });
     }
@@ -58,16 +59,68 @@ class PersonagemController {
 
       await trx.commit();
 
-      return res.status(200).send({ message: 'Personagem inserido com sucesso' });
+      return res.status(200).json({ message: 'Personagem inserido com sucesso' });
     } catch (e) {
-      trx.rollback();
+      await trx.rollback();
 
-      return res.status(404).send({ erro: e });
+      return res.status(400).json({ erro: e });
     }
   }
 
   async delete(req, res) {
-    res.send(req.body);
+    const trx = await db.transaction();
+
+    try {
+      const { id } = req.params;
+
+      const personagem = await trx('personagem').select('name').where('id', id);
+
+      if (!personagem[0]) {
+        return res.status(400).json({ errro: 'Envie um id valido' });
+      }
+
+      await trx('personagem').where('id', id).del();
+      await trx.commit();
+      return res.status(200).json({ message: 'Personagem exluido com sucesso' });
+    } catch (e) {
+      trx.rollback();
+      return res.status(400).json({ erro: 'Personagem não existe' });
+    }
+  }
+
+  async update(req, res) {
+    const { id } = req.params;
+
+    const {
+      name, age, condition, nationality, genre,
+    } = req.body;
+
+    const erros = valida(name, age, condition, nationality, genre);
+
+    if (erros.length > 0) {
+      return res.status(400).json({
+        erros: erros.map((erro) => erro),
+      });
+    }
+
+    const trx = await db.transaction();
+
+    try {
+      const personagem = await trx('personagem').select('name').where('id', id);
+
+      if (!personagem[0]) {
+        return res.status(400).json({ errro: 'Envie um id valido' });
+      }
+
+      await trx('personagem').where('id', id).update({
+        name, age, condition, nationality, genre,
+      });
+      await trx.commit();
+      return res.status(200).json({ message: 'Personagem atualizado com sucesso' });
+    } catch (e) {
+      trx.rollback();
+      return res.status(400).json({ erro: 'Personagem não existe' });
+    }
   }
 }
 
--- src/routes/personagem.js
+++ src/routes/personagem.js
@@ -4,7 +4,8 @@ import PersonagemController from '../controllers/Personagem';
 const route = new Router();
 
 route.get('/', PersonagemController.index);
-route.post('/create', PersonagemController.create);
-route.delete('/delete/:id', PersonagemController.delete);
+route.post('/', PersonagemController.create);
+route.delete('/:id', PersonagemController.delete);
+route.put('/:id', PersonagemController.update);
 
 export default route;
