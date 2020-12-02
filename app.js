970b4fbb278b2be7ae4f7ef367b0904fc7ae5adf
pedro <pedromf34@outlook.com>
2020-12-01 21:37:22 -0300

    Adicionando cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import personagem from './src/routes/personagem';
 import home from './src/routes/home';
 
 const app = express();
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public', 'images')));
